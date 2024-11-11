b7df887c8e8e50fe5db7d5870f2772a47bb5b28a
parveen barak <parveenbarak117@gmail.com>
2024-11-12 00:11:52 +0530

    code added

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "", 
+  baseURL: "https://quiz-app-2-ajym.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
